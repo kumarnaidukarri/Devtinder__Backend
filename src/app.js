630c9c2989a3d3436c00cef668dccc005285e980
Kumar <kumarnaidukarri2002@gmail.com>
2026-01-09 18:23:00 +0530

    11 - CORS library to config and fix cors errors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,9 @@ const app = express();
 
 const { connectDB } = require("./config/database.js");
 const cookieParser = require("cookie-parser"); //'cookie-parser' Library
+const cors = require("cors"); //'cors' Library
 
+app.use(cors()); // middleware fix cors, allows client requests from all origins domains
 app.use(express.json()); // middleware converts JSON data into Javascript object
 app.use(cookieParser()); // middleware parses Cookies from Request object
 
