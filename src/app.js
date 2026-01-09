d44b578a8c155e23feec828c40a2170a4b8afcb7
Kumar <kumarnaidukarri2002@gmail.com>
2026-01-09 19:45:13 +0530

    11.1 - Cors config object to accept Origins, Cookies

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ const { connectDB } = require("./config/database.js");
 const cookieParser = require("cookie-parser"); //'cookie-parser' Library
 const cors = require("cors"); //'cors' Library
 
-app.use(cors()); // middleware fix cors, allows client requests from all origins domains
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+); // middleware fix cors
+/* 'cors config object' used to allow requests from specified 'ORIGINS DOMAINS', enables Cookies/Credentials to be sent btw frontend and backend */
 app.use(express.json()); // middleware converts JSON data into Javascript object
 app.use(cookieParser()); // middleware parses Cookies from Request object
 
