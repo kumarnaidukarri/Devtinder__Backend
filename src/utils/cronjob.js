c6e2f5782dbda2862c1516f8fe9b72226e1f42d4
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-01 21:14:30 +0530

    16.1 - 'Date-Fns' library for datetime

--- src/utils/cronjob.js
+++ src/utils/cronjob.js
@@ -5,17 +5,29 @@
    "*\5 * * * * *"     means  runs for every 5 second
 */
 
-const cron = require("node-cron");
+const cron = require("node-cron"); // 'Node-Cron' Library
+const { subDays, startOfDay, endOfDay } = require("date-fns"); // 'Date-Fns' Library
 
-cron.schedule("5 * * * * *", () => {
-  // "* * * * * *" = "sec(optional) min hr date month day"
-  console.log("Hello World", new Date());
-}); // runs at 5th second of every minute  -  00:00:05, 00:01:05, 00:02:05, ...
+cron.schedule("2 * * * * *", () => {
+  // "0 0 8 * * *" = "0sec 0min 8am everyday everymonth everydayofweek". it runs once at 8th hour Every Day.
+  const yesterday = subDays(new Date(), 1);
+  const yesterdayStart = startOfDay(yesterday);
+  const yesterdayEnd = endOfDay(yesterday);
+  console.log("yesterday:-      ", yesterday.toLocaleString());
+  console.log("yesterdayStart:- ", yesterdayStart.toLocaleString());
+  console.log("yesterdayEnd:-   ", yesterdayEnd.toLocaleString());
+});
 
-cron.schedule("*/5 * * * * *", () => {
-  console.log("Hi", new Date());
-}); // runs for every 5 secs  -  0, 5, 10, 15, 20, ...
+/* COMMENTS */
+//cron.schedule("5 * * * * *", () => {
+// "* * * * * *" = "sec(optional) min hr date month day"
+//  console.log("Hello World", new Date());
+//}); // runs at 5th second of every minute  -  00:00:05, 00:01:05, 00:02:05, ...
 
-cron.schedule("0 15 */6 * * *", () => {
-  console.log("Bye", new Date());
-}); // runs for every 6 hours at minute 15  -  00:15:00, 06:15:00, 12:15:00, 18:15:00, ...
+// cron.schedule("*/5 * * * * *", () => {
+//  console.log("Hi", new Date());
+//}); // runs for every 5 secs  -  0, 5, 10, 15, 20, ...
+
+//cron.schedule("0 15 */6 * * *", () => {
+//  console.log("Bye", new Date());
+//}); // runs for every 6 hours at minute 15  -  00:15:00, 06:15:00, 12:15:00, 18:15:00, ...
