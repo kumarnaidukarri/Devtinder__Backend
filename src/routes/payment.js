0fb6a7262c5b19bc414ea7ba3d866a7d519ef99a
Kumar <kumarnaidukarri2002@gmail.com>
2026-02-05 18:57:47 +0530

    17.3 - Updated 'Payment route' to store 'order data' in Database

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -2,13 +2,14 @@ const express = require("express");
 const paymentRouter = express.Router(); // payment router
 
 const { userAuth } = require("../middlewares/auth.js"); //Authentication Middleware
+const paymentModel = require("../models/payment.js"); //payment DB model
 const razorpayInstance = require("../utils/razorpay.js"); //Razorpay integration Module
 
 // Create Order API
 paymentRouter.post("/payment/create", userAuth, async (req, res) => {
   try {
     // 1. Razorpay will create an 'order' in Razorpay Server and returns Promise
-    const order = await razorpayInstance.order.create({
+    const order = await razorpayInstance.orders.create({
       amount: 5000, // 5000 Paisa
       currency: "INR",
       receipt: "receipt#1",
@@ -22,9 +23,20 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
 
     // 2. Save it in Database
     console.log(order);
+    // creating a new document instance using Payment model
+    const payment = new paymentModel({
+      userId: req.user._id,
+      orderId: order.id,
+      status: order.status,
+      amount: order.amount,
+      currency: order.currency,
+      receipt: order.receipt,
+      notes: order.notes,
+    });
+    const savedPayment = await payment.save(); // Saves in DB
 
     // 3. Return the 'Order details' to Frontend.
-    res.json({ order });
+    res.json({ ...savedPayment.toJSON() });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ msg: err.message });
--- src/utils/razorpay.js
+++ src/utils/razorpay.js
@@ -1,4 +1,6 @@
 // Razorpay Integration file
+const Razorpay = require("razorpay"); // Razorpay library
+
 var instance = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
